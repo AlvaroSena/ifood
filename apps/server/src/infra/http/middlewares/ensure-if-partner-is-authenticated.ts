a151278050bf5835addb040fad66fb2ea26eb264
AlvaroSena <alvarosenacs.c@gmail.com>
2024-12-03 15:06:58 -0300

    refactor: check if partner exists at auth middleware

--- apps/server/src/infra/http/middlewares/ensure-if-partner-is-authenticated.ts
+++ apps/server/src/infra/http/middlewares/ensure-if-partner-is-authenticated.ts
@@ -1,9 +1,12 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { verify } from "jsonwebtoken";
 import { env } from "../../../env";
+import { PrismaPartnerRepository } from "../../repositories/prisma/prisma-partner-repository";
+import { prisma } from "../../prisma";
 
 export async function ensureIfPartnerIsAuthenticated(request: FastifyRequest, reply: FastifyReply) {
   const authHeader = request.headers.authorization;
+  const partnerRepository = new PrismaPartnerRepository(prisma);
 
   if (!authHeader) {
     return reply.status(401).send({ message: 'Token is missing!' });
@@ -12,6 +15,12 @@ export async function ensureIfPartnerIsAuthenticated(request: FastifyRequest, re
   const [, token] = authHeader.split(' ');
 
   const { sub } = verify(token, env.JWT_SECRET);
+
+  const partner = await partnerRepository.findById(sub as string);
+
+  if (!partner) {
+    return reply.status(401).send({ message: 'Usuário não autorizado' });
+  }
 
   request.id = sub as string;
 }
